19d624959094ea95ff2ccf2c70dc1523d1551e51
e2643367-ux <2.78279637e+08+e2643367-ux@users.noreply.github.com>
2026-08-27 01:22:47 +0000

    Fix GitHub Pages base routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
-function Router() {
+function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -23,7 +23,9 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+            <AppRoutes />
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
@@ -31,4 +33,3 @@ function App() {
 }
 
 export default App;
-
